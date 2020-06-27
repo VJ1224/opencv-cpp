edccb12057b31aafbe434d97b7cc48770753e6ac
Vansh <vanshjain1224@gmail.com>
2020-06-27 21:29:28 +0530

    initial

---
+++ README.md
@@ -0,0 +1,10 @@
+# OpenCV Projects in C++
+
+Projects created using the OpenCV library in C++
+
+
+## Requirements
+
+* [OpenCV] (https://github.com/opencv/opencv)
+
+* [Visual Studio] (https://visualstudio.microsoft.com/downloads/)
---
+++ face-detection/main.cpp
@@ -0,0 +1,41 @@
+#include <opencv2/opencv.hpp>
+
+using namespace std;
+using namespace cv;
+
+int main() {
+	double scale = 3.0;
+	CascadeClassifier faceCascade;
+	faceCascade.load("C:\\Users\\Vansh Jain\\projects\\c\\opencv\\install\\etc\\haarcascades\\haarcascade_frontalface_default.xml");
+	
+	VideoCapture capture(0);
+	if (!capture.isOpened()) {
+		return 1;
+	}
+
+	while (1) {
+		Mat frame;
+		capture >> frame;
+
+		Mat grayscale;
+		cvtColor(frame, grayscale, COLOR_BGR2GRAY);
+		resize(grayscale, grayscale, Size(grayscale.size().width / scale, grayscale.size().height / scale));
+
+		vector<Rect> faces;
+		faceCascade.detectMultiScale(grayscale, faces, 1.1, 3, 0, Size(30, 30));
+
+		for (Rect area : faces) {
+			Scalar colour = Scalar(0, 255, 0);
+			int thickness = 3;
+			rectangle(frame, Point(cvRound(area.x * scale), cvRound(area.y * scale)),
+				Point(cvRound((area.x + area.width - 1) * scale), cvRound(area.y + area.height - 1) * scale), 
+				colour, thickness);
+		}
+
+		imshow("Webcam", frame);
+
+		if (waitKey(30) >= 0) {
+			exit(0);
+		}
+	}
+}
