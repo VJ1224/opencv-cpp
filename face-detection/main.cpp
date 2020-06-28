9ce213e43ab44f0324445467d108e69bd5dbb5cc
Vansh Jain <vanshjain1224@gmail.com>
2020-06-28 19:35:40 +0530

    Saves video and image

--- face-detection/main.cpp
+++ face-detection/main.cpp
@@ -13,6 +13,14 @@ int main() {
 		return 1;
 	}
 
+	int frame_width = capture.get(CAP_PROP_FRAME_WIDTH);
+	int frame_height = capture.get(CAP_PROP_FRAME_HEIGHT);
+	VideoWriter video("data/movement.avi", VideoWriter::fourcc('M', 'J', 'P', 'G'), 5, Size(frame_width, frame_height));
+	
+	vector<int> compression_params;
+	compression_params.push_back(IMWRITE_PNG_COMPRESSION);
+	compression_params.push_back(9);
+
 	while (1) {
 		Mat frame;
 		capture >> frame;
@@ -28,14 +36,21 @@ int main() {
 			Scalar colour = Scalar(0, 255, 0);
 			int thickness = 3;
 			rectangle(frame, Point(cvRound(area.x * scale), cvRound(area.y * scale)),
-				Point(cvRound((area.x + area.width - 1) * scale), cvRound(area.y + area.height - 1) * scale),
+				Point(cvRound(area.x + area.width - 1) * scale, cvRound(area.y + area.height - 1) * scale),
 				colour, thickness);
+
+			video.write(frame);
+			imwrite("data/face.png", frame, compression_params);
 		}
 
 		imshow("Webcam", frame);
 
 		if (waitKey(30) >= 0) {
-			exit(0);
+			break;
 		}
 	}
+
+	capture.release();
+	video.release();
+	destroyAllWindows();
 }
